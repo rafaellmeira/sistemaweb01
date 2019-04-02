5b5bd53dce8f120c6ad72089d39699f8dd416564
Rafael Meira <rafaellmeirask8@gmail.com>
2019-04-02 18:26:28 -0300

    criamos a classe page

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -1,65 +1,69 @@
- <?php 
+<?php 
+
 namespace Hcode;
 
-
 use Rain\Tpl;
-
 
 class Page {
 
 	private $tpl;
-	private $options = []; 
+	private $options = [];
 	private $defaults = [
-		"data"=>[]	
-
+		"header"=>true,
+		"footer"=>true,
+		"data"=>[]
 	];
 
-	public function __construct($opts = array()){
+	public function __construct($opts = array())
+	{
 
 		$this->options = array_merge($this->defaults, $opts);
 
 		$config = array(
-					"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
-					"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/viwews-cache/",
-					"debug"         => false
-				   );
+		    "base_url"      => null,
+		    "tpl_dir"       => $_SERVER['DOCUMENT_ROOT']."/views/",
+		    "cache_dir"     => $_SERVER['DOCUMENT_ROOT']."/views-cache/",
+		    "debug"         => false
+		);
 
-	Tpl::configure( $config );
+		Tpl::configure( $config );
 
-	$this->tpl = new Tpl;
+		$this->tpl = new Tpl();
 
-	$this->setData($this->options["data"]); 
+		if ($this->options['data']) $this->setData($this->options['data']);
 
-	$this->tpl->draw("header");
-
-	}
-
-	private function setData($data = array()){
-
-	foreach ($data as $key => $value) {
-		$this->tpl->assign($key, $value);
-		}  
+		if ($this->options['header'] === true) $this->tpl->draw("header", false);
 
 	}
 
-	public function setTpl($name, $data = array(), $returnHTML = false)
+	public function __destruct()
+	{
+
+		if ($this->options['footer'] === true) $this->tpl->draw("footer", false);
+
+	}
+
+	private function setData($data = array())
+	{
+
+		foreach($data as $key => $val)
+		{
+
+			$this->tpl->assign($key, $val);
+
+		}
+
+	}
+
+	public function setTpl($tplname, $data = array(), $returnHTML = false)
 	{
 
 		$this->setData($data);
 
-		return $this->tpl->draw($name, $returnHTML);
-
-	}
-
-	public function __destruct(){
-
-		$this->tpl->draw("footer");
+		return $this->tpl->draw($tplname, $returnHTML);
 
 	}
 
 }
 
-
-
-
-  ?>
+ ?>
